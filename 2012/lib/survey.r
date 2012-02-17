5fb3e95bc7e4203de213ceee962802449c25ee22
Michael Barton <mail@michaelbarton.me.uk>
2012-02-17 15:58:20 -0500

    Include function for 2008 data

--- 2012/lib/survey.r
+++ 2012/lib/survey.r
@@ -1,9 +1,17 @@
-survey <- function(){
-  within(read.csv('data/survey.csv',stringsAsFactors = FALSE),{
-    Timestamp <- format.date.string(Timestamp)
+survey.2012 <- function(){
+  survey('data/survey.csv')
+}
+
+survey.2008 <- function(){
+  survey('../2008/data/survey.csv')
+}
+
+survey <- function(file){
+  within(read.csv(file, stringsAsFactors = FALSE),{
+    Timestamp <- format.date.string(Timestamp, "%m/%d/%Y %H:%M:%S")
   })
 }
 
-format.date.string <- function(x){
-  as.Date(x, "%m/%d/%Y %H:%M:%S")
+format.date.string <- function(x,expr){
+  as.Date(x, expr)
 }
