b32ed8d8e867b3410181b01fb3043f52eccdd60f
Michael Barton <mail@michaelbarton.me.uk>
2012-02-17 17:22:17 -0500

    Plot total submissions over time

---
+++ 2012/plot/submissions_vs_time.R
@@ -0,0 +1,35 @@
+#!/usr/bin/env Rscript
+
+library(plyr)
+library(ggplot2)
+
+source('lib/survey.r')
+
+responses <- function(data,year){
+  d <- ddply(data, .(Timestamp), function(df){
+    data.frame(responses = nrow(df["Timestamp"]), year = year)
+  })
+  within(d,{
+    Timestamp <- as.numeric(Timestamp)
+    Timestamp <- Timestamp - min(Timestamp)
+    responses <- cumsum(responses)
+  })
+}
+
+
+p <- ggplot(
+      rbind(
+        responses(survey.2008(),2008),
+        responses(survey.2012(),2012)),
+      aes(x = Timestamp, y = responses, col = factor(year)))
+
+p <- p + geom_smooth()
+p <- p + geom_point()
+p <- p + theme_bw()
+p <- p + scale_x_continuous("Days")
+p <- p + scale_y_continuous("Total responses")
+p <- p + scale_colour_manual(name = "Year",values = c("red","blue"))
+
+png('submissions_by_year.png',width = 550)
+print(p)
+dev.off()
